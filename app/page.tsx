b08ad2891d8be566bd62b0b41f98db20f0600c1b
kss2002 <sc00310@naver.com>
2025-09-20 16:14:04 +0900

    magic ui add

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
+import { BorderBeam } from '@/components/ui/border-beam';
+import { TypingAnimation } from '@/components/ui/typing-animation';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center text-center py-20">
       <div
-        className="w-full max-w-6xl mx-auto border border-gray-800 rounded-2xl p-24"
+        className="relative overflow-hidden w-full max-w-6xl mx-auto border border-gray-800 rounded-2xl p-24"
         style={{
           backgroundImage: 'url(/Rectangle-home.png)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
       >
+        <BorderBeam duration={12} size={120} />
+
         <h1 className="text-6xl font-bold text-white mb-4">
           Invest in Authenticity
         </h1>
@@ -20,7 +24,7 @@ export default function Home() {
         </p>
         <Link href="marketplace">
           <button
-            className="text-white font-bold py-3 px-21 rounded-full text-lg hover:shadow-xl/40 shadow-red-500/50"
+            className="text-white font-bold py-3 px-21 rounded-full text-lg  hover:shadow-xl/40 shadow-red-500/50 "
             style={{
               backgroundImage: 'url(/pain.png)',
               backgroundSize: 'cover',
