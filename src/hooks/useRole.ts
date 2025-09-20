767180ca3d95e15b986d81c59f0e6f180c133397
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-09-20 15:35:32 +0900

    feat(dashboard): general dashboard 구현 (#12)

--- src/hooks/useRole.ts
+++ src/hooks/useRole.ts
@@ -5,6 +5,6 @@ import { ROLES } from '../constants/role';
 
 export const useRole = () => {
   // TODO: 유저 정보 조회 API 연동
-  const [role, setRole] = useState<keyof typeof ROLES>('GALLERY');
+  const [role, setRole] = useState<keyof typeof ROLES>('USER');
   return role;
 };
