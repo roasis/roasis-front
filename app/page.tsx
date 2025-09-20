7cc782968f109f3cfdaeddd166a597e3697f60d3
kss2002 <sc00310@naver.com>
2025-09-20 16:33:09 +0900

    import문 지움

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { BorderBeam } from '@/components/ui/border-beam';
-import { TypingAnimation } from '@/components/ui/typing-animation';
 import Link from 'next/link';
 
 export default function Home() {
