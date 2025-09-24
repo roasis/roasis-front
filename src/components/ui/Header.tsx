db2163aa7b0bc5d649ba0aca288012957bbea3cb
kss2002 <sc00310@naver.com>
2025-09-24 12:11:04 +0900

    header에도 적용 및 언어 스위치 버튼 추가

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -4,36 +4,40 @@ import WalletConnectButton from '../wallet/WalletConnectButton';
 import WalletDisconnectButton from '../wallet/WalletDisconnectButton';
 import Link from 'next/link';
 import Image from 'next/image';
+import LanguageSwitcher from './LanguageSwitcher';
+import { useTranslations, useLocale } from 'next-intl';
 
 export default function Header() {
   const session = useSession();
+  const t = useTranslations('Header');
+  const locale = useLocale();
 
   return (
     <header className="flex items-center justify-around p-6 text-white">
       <div className="flex items-center space-x-8">
-        <Link href="/">
+        <Link href={`/${locale}`}>
           <Image src="/roasis.png" alt="logo" width={100} height={28} />
         </Link>
         <nav className="flex items-center space-x-6 text-sm">
-          <Link href="/marketplace" className="hover:text-gray-300">
-            Marketplace
+          <Link href={`/${locale}/marketplace`} className="hover:text-gray-300">
+            {t('marketplace')}
           </Link>
-          <Link href="/artists" className="hover:text-gray-300">
-            Artists
+          <Link href={`/${locale}/artists`} className="hover:text-gray-300">
+            {t('artists')}
           </Link>
-          <Link href="/galleries" className="hover:text-gray-300">
-            Galleries
+          <Link href={`/${locale}/galleries`} className="hover:text-gray-300">
+            {t('galleries')}
           </Link>
-
-          <Link href="/dashboard" className="hover:text-gray-300">
-            Dashboard
+          <Link href={`/${locale}/dashboard`} className="hover:text-gray-300">
+            {t('dashboard')}
           </Link>
         </nav>
       </div>
       <div className="flex items-center space-x-4">
+        <LanguageSwitcher />
         <input
           type="text"
-          placeholder="Search art, artists, galleries"
+          placeholder={t('searchPlaceholder')}
           className="w-64 bg-transparent border border-gray-600 rounded-full px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-gray-500"
         />
         {session ? (
